b10203b4a26c5774d517527efb318a2a5de9fcbf
vmdivyareddy18 <vmdivyareddy18@gmail.com>
2026-02-16 22:55:14 +0530

    new

--- script.js
+++
@@ -1,9 +0,0 @@
-function bookRoom(roomName) {
-    alert("You booked: " + roomName);
-}
-
-function scrollToRooms() {
-    document.getElementById("rooms").scrollIntoView({
-        behavior: "smooth"
-    });
-}
